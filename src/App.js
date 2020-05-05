5e14146155ccd9b5daa31597c60241f3db0667c4
danjones1618 <hello@danjones.dev>
2020-05-05 22:53:14 +0100

    Add TA to timeline

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import {
   faArrowLeft,
   faEgg,
   faPlus,
+  faChalkboardTeacher,
 } from '@fortawesome/free-solid-svg-icons'
 import SideBar from './SideBar.js';
 import Carosell from './Carosell.js';
@@ -23,7 +24,7 @@ import road from './res/road.jpg';
 import turbine from './res/windTurbine.jpg';
 
 library.add(far, fab, faEnvelope, faSquare, faUniversity, faSchool,
-  faNewspaper, faIdBadge, faArrowLeft, faEgg, faPlus);
+  faNewspaper, faIdBadge, faArrowLeft, faEgg, faPlus, faChalkboardTeacher);
 
 function HomeSlide() {
   return (
@@ -37,6 +38,14 @@ function HomeSlide() {
         text: ["CSS Webmaster", "I'm currently the webmaster for our subject's Society"],
       }
     ]}/>
+  );
+}
+
+function SkillsHighlight() {
+  return (
+    <section id="Skills-Highlight">
+      <h3></h3>
+    </section>
   );
 }
 
@@ -54,9 +63,13 @@ function TimeLineSection() {
           <p>Achieved for demonstrating gained employability skills and taking part in extra-curricular activities to help prepare me for my future career.</p>
           <a href="https://bristol.ac.uk/careers/bristol-plus-award/">Find out more</a>
         </TimelineItem>
+        <TimelineItem icon={faChalkboardTeacher} date="24/01/2020 - 30/06/2020">
+          <h4>Algorithms TA</h4>
+          <p>In the second semester of my second year, I was a teaching assistant for the first year Algorithms module</p>
+        </TimelineItem>
         <TimelineItem icon={faEgg} date="22/11/2019">
           <h4>2019 CSS GameJam</h4>
-          <p>In a team of 4 created a html5 game called <em>"Mumma's Eggaria"</em> and came 2nd place!</p>
+          <p>In a team of 4 created a HTML5 game called <em>"Mumma's Eggaria"</em> and came 2nd place!</p>
           <p>The game is inspired by the Papa's flash game saga. In this game you are a chef in an Egg Cafe and have to prepare various egg-based dishes</p>
           <a href="https://danjones.dev/mummas-eggaria">Play it here</a>
         </TimelineItem>
