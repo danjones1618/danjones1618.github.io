f1438f8ed3609255c6ca8e3a067adb933d1e43a5
danjones1618 <hello@danjones.dev>
2021-09-06 22:41:20 +0100

    Convert JS files to TS

--- components/date.tsx
+++ components/date.tsx
@@ -1,6 +1,9 @@
+import { FC } from 'react';
 import { parseISO, format } from 'date-fns';
 
-export default function Date({ dateString }) {
+export const Date : FC<{dateString: string}> = ({ dateString }) => {
   const date = parseISO(dateString);
   return <time dateTime={dateString}>{format(date, 'do LLLL yyyy')}</time>
 }
+
+export default Date;
